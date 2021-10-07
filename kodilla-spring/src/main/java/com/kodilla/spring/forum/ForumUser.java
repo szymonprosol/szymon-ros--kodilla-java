2d3b331fd46fbf3815482b29f6b2a0caba1eb2ae
Szymon Rosół <szymon.patryk.rosol@gmail.com>
2021-10-07 19:49:53 +0200

    Added ForumUser and ForumUserTestSuite classes

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+
+    }
+}

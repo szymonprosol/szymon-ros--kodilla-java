e5fc199cec1098d3c52d50bae1ef97791a901e83
Szymon Rosół <szymon.patryk.rosol@gmail.com>
2021-12-21 04:20:38 +0100

    Bookclassifier package has been added

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapter.java
@@ -0,0 +1,26 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.Classifier;
+import com.kodilla.patterns2.adapter.bookclassifier.libraryb.BookSignature;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MedianAdapter extends MedianAdaptee implements Classifier {
+
+    @Override
+    public int publicationYearMedian(Set<Book> bookSet) {
+
+        Map<BookSignature, com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book> books = new HashMap<>();
+
+        for(Book book : bookSet) {
+            com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book bookB = new com.kodilla.patterns2.adapter.bookclassifier.libraryb.Book(
+                    book.getAuthor(), book.getTitle(), book.getPublicationYear());
+            BookSignature bookSignature = new BookSignature(book.getSignature());
+            books.put(bookSignature, bookB);
+        }
+        return medianPublicationYear(books);
+    }
+}

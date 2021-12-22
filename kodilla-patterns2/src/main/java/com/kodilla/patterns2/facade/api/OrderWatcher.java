a32d5881b0026592a6cc674c6ecd345445b7c623
Szymon Rosół <szymon.patryk.rosol@gmail.com>
2021-12-22 05:23:47 +0100

    OrderWatcher class has been changed

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderWatcher.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderWatcher.java
@@ -23,15 +23,10 @@ public class OrderWatcher {
     @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
     public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         Object result;
-        //try {
             long begin = System.currentTimeMillis();
             result = proceedingJoinPoint.proceed();
             long end = System.currentTimeMillis();
             LOGGER.info("Time consumed " + (end - begin) + "[ms]");
-        //} catch (Throwable throwable) {
-        //    LOGGER.error(throwable.getMessage());
-        //    throw throwable;
-        //}
         return result;
     }
 }

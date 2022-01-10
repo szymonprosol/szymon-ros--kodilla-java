606c4be0d1436e164150950d1143e9a362d481e5
Maciej Rudnicki <maciejru@amazon.com>
2022-01-10 19:45:33 +0100

    Proposal of solution

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/loty/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/loty/Application.java
@@ -1,15 +1,11 @@
 package com.kodilla.good.patterns.challenges.loty;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Application {
 
     public static void main(String[] args) {
 
-        FlightService flightService = new FlightService(new FlightRequestRetriever().retrieve());
-        flightService.posibleFlight();
+        FlightService flightService = new FlightService();
+
+        flightService.printInterchangeAirports("Warszawa Okęcie","Modlin" );
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/loty/FlightService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/loty/FlightService.java
@@ -1,37 +1,66 @@
 package com.kodilla.good.patterns.challenges.loty;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FlightService {
 
-    private final FlightRequest flightRequest;
     private List<Airport> airports = new FlightHolder().createAirports();
 
-    public FlightService(FlightRequest flightRequest) {
-        this.flightRequest = flightRequest;
+    public void printFlightsFromDepartureAirport(Airport departureAirport){
+        departureAirport.getDestinationAirports().stream()
+                .map(airport -> airport.getAirportName())
+                .forEach(airport -> System.out.println(airport));
     }
 
-    public void posibleFlight() {
+    public void printFlightsToAirport(Airport arrivalAirport) {
 
-        if (flightRequest.getDepartureAirport() == null && flightRequest.getDestinationAirport() == null) {
-
-            System.out.println("Brak możliwoci wyszukania lotów");
-        } else if (flightRequest.getDestinationAirport() == null) {
-
-            airports.stream()
-                    .filter(airport -> airport.getAirportName() == flightRequest.getDepartureAirport())
-                    .map(airport -> airport.getDestinationAirports())
-                    .flatMap(a -> a.stream()
-                            .map(b -> b.getAirportName())).forEach(System.out::println);
-        } else if (flightRequest.getDepartureAirport() == null && flightRequest.isDirectFlight()) {
-
-            airports.stream()
-                    .map(a -> a.getDestinationAirports())
-                    .flatMap(a -> a.stream()
-                            .map(b -> b.getAirportName()))
-                    .filter(c -> c != flightRequest.getDestinationAirport())
-                    .forEach(System.out::println);
+        for (Airport airport : airports) {
+            if (airport.getDestinationAirports().contains(arrivalAirport)) {
+                System.out.println(airport.getAirportName());
+            }
         }
     }
+
+    //Gdańska przez <Kraków> do Wrocławia.
+    public void printInterchangeAirports(String departureAirportName, String arrivalAirportName){
+
+        Airport departureAirport = airports.stream()
+                .filter(airport -> airport.getAirportName().equals(departureAirportName))
+                .findAny().orElseThrow();
+        Airport arrivalAirport =  airports.stream()
+                .filter(airport -> airport.getAirportName().equals(arrivalAirportName))
+                .findAny().orElseThrow();
+
+        for(Airport interchangeAirport : departureAirport.getDestinationAirports()){
+            if(interchangeAirport.getDestinationAirports().contains(arrivalAirport)){
+                System.out.println("Departure "+departureAirport+", interchange "+interchangeAirport+", arrival "+arrivalAirportName);
+            }
+        }
+    }
+
+
+
+//    public void printPossibleFlights(FlightRequest flightRequest) {
+//
+//        if (flightRequest.getDepartureAirport() == null && flightRequest.getDestinationAirport() == null) {
+//
+//            System.out.println("Brak możliwoci wyszukania lotów");
+//        } else if (flightRequest.getDestinationAirport() == null) {
+//
+//            airports.stream()
+//                    .filter(airport -> airport.getAirportName() == flightRequest.getDepartureAirport())
+//                    .map(airport -> airport.getDestinationAirports())
+//                    .flatMap(a -> a.stream()
+//                            .map(b -> b.getAirportName())).forEach(System.out::println);
+//        } else if (flightRequest.getDepartureAirport() == null && flightRequest.isDirectFlight()) {
+//
+//            airports.stream()
+//                    .map(a -> a.getDestinationAirports())
+//                    .flatMap(a -> a.stream()
+//                            .map(b -> b.getAirportName()))
+//                    .filter(c -> c != flightRequest.getDestinationAirport())
+//                    .forEach(System.out::println);
+//        }
+//    }
 }
